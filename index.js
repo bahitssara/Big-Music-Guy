33413e2bf2961e0be21b6e488bcdf242bfe48e8c
Sara Mayberry <saraamayberry@yahoo.com>
2019-02-20 10:16:16 -0700

    news API

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
         //give value to input to be re-used throughout//
         const searchInput = $('.search-bar').val();
         getYoutubeResults(searchInput);
+        getNewsResults(searchInput);
      })
  }
 
@@ -56,6 +57,44 @@ $(document).ready(function(){
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
     })
  }
+
+ // fetch news articles
+ function getNewsResults(query){
+    const params = {
+        q: query,
+        maxResults: 6,
+        language: 'en'
+    };
+    const apiKey = '3311ba7deadd4135ac85e662cfb2f5fe';
+    const newsUrl = 'https://newsapi.org/v2/everything';
+    const queryString = setQueryParams(params)
+    const url = newsUrl + '?' + queryString;
+
+    const options = {
+       headers: new Headers({
+         "X-Api-Key": apiKey})
+     };
+
+     fetch(url, options)
+       .then (response => {
+           if (response.ok) {
+               return response.json();
+           }
+           throw new Error(response.statusText);
+       })
+       .then(responseJson => {
+           console.log(responseJson)
+       }) 
+       .catch(err => {
+           $('#js-error-message').text(`Something went wrong: ${err.message}`);
+   })
+}
+
+
+
+
+
+
 
  $(watchForm);
 
