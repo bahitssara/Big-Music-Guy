101a35403cda97ae802b48e6c749e219a5df13e2
Sara Mayberry <saraamayberry@yahoo.com>
2019-03-01 22:28:17 -0700

    css/js-edit

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
 
     responseJson.items.forEach(video => {
         $('.youtube-results').append(`
-        <h3 class="youtube-title">${video.snippet.title}</h3>
+        <h3 class="youtube-title">${video.snippet.title}</a></h3>
                 <div class="iFrame">
                     <iframe class="iFrame-box" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allowfullscreen></iframe><br>
            </div>    
@@ -116,9 +116,8 @@ function renderNewsResults(responseJson){
 
     responseJson.articles.forEach(news => {
         $('.news-results').append(`
-            <h3 class="news-title">${news.title}</h3>
+            <h3 class="news-title"><a href="${news.url}" target="_blank">${news.title}</a></h3>
             <img src="${news.urlToImage}" class="news-image" alt="news image">
-            <a href="${news.url}" target="_blank">Read Full Article</a>
         `)
     })
 
@@ -141,6 +140,9 @@ function getTasteDiveResults(query,callback) {
     $.getJSON(url,callback, function(json){
         renderTasteDiveResults(json);
     }) 
+    .error(function() { 
+        alert("Something went wrong, try again!"); 
+    })
 }
 
 //display tastedive results
@@ -151,9 +153,11 @@ function renderTasteDiveResults(json) {
     json.Similar.Results.forEach(rec => {
         $('.tastedive-results').append(`
         <div class="tastedive-list">
-            <h3>${rec.Name}</h3>
-            <a href="${rec.yUrl}" target="_blank">Video</a>
-            <a href="${rec.wUrl}" target="_blank">Wiki</a> 
+            <h3><a href="${rec.wUrl}" target="_blank">${rec.Name}</a></h3>
+            <div class="iFrame">
+                <iframe class="iFrame-box" src="${rec.yUrl}" frameborder="0" allowfullscreen></iframe><br>
+           </div>   
+           <p class="wiki-results">${rec.wTeaser}</p> 
         </div>
         `)
     })
@@ -161,16 +165,24 @@ function renderTasteDiveResults(json) {
     $('.results').removeClass('hidden');
 }
 
-$('.search-again').click(function() {
+//Search again button-Scrolls to top of page
+function searchAgainButton() {
+    $('.search-again').click(function() {
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
 });
+}
 
+//search button aligns you with results with each search
+function searchButtonOffset(){
 $('#search-button').click(function() {
 $('html, body').animate({
     scrollTop: ($('.results-page').offset().top)
 },500);
 });
+}
 
 
  $(watchForm);
+ $(searchAgainButton);
+ $(searchButtonOffset);
